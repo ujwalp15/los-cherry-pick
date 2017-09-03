48cbde6fc5db385933b8f225a513fc001cc15bb8
ujwalp15 <ujwalp09@gmail.com>
2017-09-03 12:37:03 +0530

    cherry-pick: add initial picks

---
+++ cherry-pick.sh
@@ -0,0 +1,66 @@
+echo "cherry-picking bionic commits"
+cd bionic
+
+repopick 185990 185989 186050 186051 185636 185640
+
+echo "bionic commits picked successfully"
+
+echo "cherry-picking build commits"
+
+repopick 186687 185913 187329 187766 187894 187330 187331 187332 187372 187896 187373 187374 187375
+
+echo "build commits picked successfully"
+
+echo "cherry-picking soong commits"
+
+repopick 186003 186004 186005 186006 186074 186075
+
+echo "soong commits picked successfully"
+
+echo "cherry-picking av commits"
+
+repopick 185875 185876 187558 187559 187560 187561 186580 186581 186712
+
+echo "av commits picked successfully"
+
+echo "cherry-picking fwb commits"
+
+repopick 186228 187154 187851
+
+echo "fwb commits picked successfully"
+
+echo "cherry-picking native commits"
+
+repopick 185879 185880 185881 185882 187152
+
+echo "native commits picked successfully"
+
+echo "cherry-picking libhardware commits"
+
+repopick 187643 187644
+
+echo "libhardware commits picked successfully"
+
+echo "cherry-picking system/sepolicy commits"
+
+repopick 186243 186244 186245 186246
+
+echo "system/sepolicy commits picked successfully"
+
+echo "cherry-picking system/core commits"
+
+repopick 185888 186304 185653 185656 185657 185658 186881 187146 185642
+
+echo "system/core commits picked successfully"
+
+echo "cherry-picking system/qcom commits"
+
+repopick 187634 187635 187636 187637
+
+echo "system/qcom commits picked successfully"
+
+echo "cherry-picking vendor/lineage commits"
+
+repopick 185491 187574 185521
+
+echo "vendor/lineage commits picked successfully"
