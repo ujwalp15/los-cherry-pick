af26a1f992612a7fac597c1398b4080a7d8e9166
ujwalp15 <ujwalp09@gmail.com>
2017-09-17 11:41:16 +0530

    cherry-pick: remove merged commits and add new ones

--- cherry-pick.sh
+++ cherry-pick.sh
@@ -1,9 +1,5 @@
 # bionic
-repopick 185636 # linker: Make platform text relocations denial optional
 repopick 185640 # linker: Add support for dynamic SHIM libraries
-
-# bootable/recovery
-repopick 186286 # Revert "updater: Remove some obsoleted functions for file-based OTA."
 
 # build
 repopick 187372 #edify: bring back SetPermissionsRecursive
@@ -15,23 +11,8 @@ repopick 187373 #Edify: Add abort message for bootloader asserts
 repopick 187374 #releasetools: ota_from_target_files: add FullOTA_PostValidate
 repopick 187375 #releasetools: allow devices to specify a custom make_recovery_patch
 
-# build/soong
-repopick 188279 # soong: add support for nested structs in variableProperties
-repopick 188280 # soong: explicitly specify name of Lineage variables struct
-
-# external/tinycompress
-repopick 185936 # tinycompress: squash tinycompress fixes
-repopick 188064 # tinycompress: squash vendor specific tinycompress changes
-repopick 185937 # audio: compress error propagation
-repopick 185938 # compress: Extend cplay with support to offload path
-#185939 # tinycompress: Fix compilation on old targets
-repopick 189132 # tinycompress: Generate vendor specifc tinycompress
-
 # framework/base
 repopick 187851 # Enable NSRM (Network Socket Request Manager).
-
-# framework/native
-repopick 185671 # native: Restore VM memory overrides
 
 # framework/opt/telephony
 repopick 188687 # telephony: Squash support for simactivation feature
@@ -58,25 +39,17 @@ repopick 187874 # Wifi: Quiet some excessive debug output
 # packages/apps/FMRadio
 repopick 186688 # FMRadio: jni: Add missing liblog dependency
 
-# packages/providers/TelephonyProvider
-repopick 185944 # telephony: Add support for choosing a default APN
-repopick 185945 # TelephonyProvider: Fix Default APN Preference Overlay on 1st Boot
-
 # packages/services/Telephony
 repopick 187147 # Fix MobileNetworkSettings crash for multi-SIM device
 
 # system/core
 repopick 188765 # init: Fire a trigger when a class is started/stopped
+repopick 189293 # Revert "cutils: Add support for ioprio cgroup"
+repopick 189604 # ueventd: Add bootdevice symlink for boot storage device
+repopick 189605 # Init: Support bootdevice symlink for early mount.
 
 # system/qcom
 repopick 187634 # qsap: Don't log sensitive information
 repopick 187635 # qsap: Put files in a sane location
 repopick 187636 # qsap: Kill more logspam
 repopick 187637 # Implement wifi_qsap_set_tx_power
-
-# vendor/lineage
-repopick 187951 # lineage: healthd: add CFLAG around power off alarm via legacy alarm driver
-repopick 185521 # libhealthd: Add libbase and libminui static libraries
-repopick 185491 # overlay: Disable unusupported breaking overlays
-repopick 188278 # build: soong: put lineage variables in the "Lineage" struct
-repopick 185870 # soong_config: Add TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS
