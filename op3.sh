08ba5325126c25fda35bad92539195a4eaafde32
Ujwal P <ujwalp09@gmail.com>
2019-09-06 10:10:48 +0530

    op3: add picks for op3

---
+++ op3.sh
@@ -0,0 +1,8 @@
+# oppo-common stuff
+repopick 255668 #oppo-common: Remove LOCAL_MODULE_TAGS := eng usage
+repopick 255863 #hax
+
+# op3 stuff
+repopick 255737 #[DNM] op3: disable selinux
+repopick 255744 #op3: Remove LOCAL_MODULE_TAGS := eng usage
+repopick 255743 #hax
